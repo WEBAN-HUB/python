b07e6c8200357c27dc11cc19ee8cfc535935c3d3
WEBAN <zkzk1231@gmail.com>
2024-09-02 17:45:55 +0900

    2562번의 좀 더 깔끔한 풀이

--- 2562.py
+++ 2562.py
@@ -1,16 +1,5 @@
-import sys
-
-nlist = []
-
-for i in range(0,9):
-    N = int(sys.stdin.readline())
-    nlist.append(N)
-
-N = 0
-index = 0
-
-for idx, value in enumerate(nlist):
-    if N < value:
-        N = value
-        index = idx
-print(str(N),index+1,sep="\n")
+l = []
+for i in range(9):
+    l.append(int(input()))
+print(max(l))
+print(l.index(max(l))+1)
