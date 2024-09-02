2d1c1632a4b2646dddbd40167cd164072da7b679
WEBAN <zkzk1231@gmail.com>
2024-09-02 19:43:42 +0900

    10813 파이썬 스타일에 맞게 수정

--- 10813.py
+++ 10813.py
@@ -1,8 +1,6 @@
 N, M = map(int,input().split())
-nlist = [i + 1 for i in range(N)]
+nlist = list(range(1,N+1))
 for i in range(M):
     i, j = map(int,input().split())
-    temp = nlist[i-1]
-    nlist[i-1] = nlist[j-1]
-    nlist[j-1] = temp
+    nlist[i-1], nlist[j-1] = nlist[j-1], nlist[i-1]
 print(*nlist,sep=" ")
