6e30ccce7c720ff476f8cf23f3291acddb4c417c
WEBAN <zkzk1231@gmail.com>
2024-09-02 20:19:49 +0900

    5597 로직 개선

--- 5597.py
+++ 5597.py
@@ -1,10 +1,8 @@
-studentList = list(range(1,31))
-nList = []
-printN = []
-for i in range(1,29):
-    nList.append(int(input()))
-for value in studentList:
-    if value not in nList:
-        printN.append(value)
-printN.sort()
-print(*printN, sep="\n")
+a=list()
+for i in range(28):
+  m=int(input())
+  a.append(m)
+
+for i in range(1,31):
+  if i not in a:
+    print(i)
