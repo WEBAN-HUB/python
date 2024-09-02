1517aeb58ffc6b7c9e3f2b8016783f02fe28ff2c
WEBAN <zkzk1231@gmail.com>
2024-09-02 21:27:11 +0900

    10811 파이썬 스타일에 맞게 로직 수정

--- 10811.py
+++ 10811.py
@@ -1,12 +1,6 @@
-N, M = map(int,input().split())
-nList = list(range(1,N+1))
-
-for index in range(0,M):
-    i, j = map(int,input().split())
-    rList = []
-    for num in range(i-1, j):
-        rList.append(nList[num])
-    rList.reverse()
-    for index, value in enumerate(range(i-1, j)):
-        nList[value] = rList[index]
-print(*nList)
+N, M = map(int, input().split())
+nList = [i for i in range(1, N + 1)]
+for _ in range(M):
+    i, j = map(int, input().split())
+    nList[i - 1 : j] = reversed(nList[i - 1 : j])
+print(*nList, sep=" ")
