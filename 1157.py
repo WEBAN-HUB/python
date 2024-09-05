bb85e4790b08bd6c04f3ef3d6912a67b90cf8a7c
WEBAN <zkzk1231@gmail.com>
2024-09-05 19:26:36 +0900

    1157 문제 : 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
    입력 : 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
    
    출력 : 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
    
    오답의 이유 : 문제 조건 중 시간 제한 2초를 파악하지 못했다.
    입력 조건의 단어 길이제한을 보면 1,000,000이기 때문에 문자열을 다 순회하다보면 시간 조건을 충족 시킬 수 없는 것이 당연헀다.
    set으로 중복 제거한 리스트를 순회하도록 하여 시간 조건을 해결하였다.

--- 1157.py
+++ 1157.py
@@ -1,10 +1,12 @@
 s = input().upper()
-max = ""
-count = 0
-for value in s:
-    if s.count(value) > count:
-        max = value
-        count = s.count(value)
-    elif s.count(value) == count and max != value:
-        max = '?'
-print(max)
+sSet = list(set(s))
+max = 0
+chara = ""
+for value in sSet:
+    count = s.count(value)
+    if max < count:
+        chara = value
+        max = count
+    elif max == count:
+        chara = "?"
+print(chara)
