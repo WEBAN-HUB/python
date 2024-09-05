a3f98d7c381b2e7c376611e43058dd5a3f3233c6
WEBAN <zkzk1231@gmail.com>
2024-09-05 20:30:21 +0900

    1316 개선 버전

--- 1316.py
+++ 1316.py
@@ -1,16 +1,10 @@
-N = int(input())
-group = 0
-for i in range(N):
-    s = input()
-    chkGroup = []
-    chkChr = ""
-    add = 1
-    for value in s:
-        if chkChr != value:
-            chkChr = value
-            if value not in chkGroup:
-                chkGroup.append(value)
-            else:
-                add = 0
-    group += add
-print(group)
+n=int(input())
+for i in range(n):
+	word=input()
+	for j in range(len(word)-1):
+		if word[j]==word[j+1]:
+			pass
+		elif word[j] in word[j+1:]:
+			n-=1
+			break
+print(n)
